6199903d3a73a72487469a812134c56cc08ac05e
EniatinusJr <maurin@schucan.ch>
2026-07-14 15:26:19 +0200

    complete chapter 7

---
+++ estate/models/estate_property_offer.py
@@ -0,0 +1,15 @@
+from odoo import fields, models
+
+class PropertyOffer(models.Model):
+    _name = 'estate.property.offer'
+    _description = 'Real Estate Property Offer'
+
+    price = fields.Float('Price')
+    status = fields.Selection(
+        string='Status',
+        selection=[('accepted', 'Accepted'), ('refused', 'Refused')],
+        help='Status of the offer',
+        copy=False,
+    )
+    partner_id = fields.Many2one('res.partner', string='Partner', required=True)
+    property_id = fields.Many2one('estate.property', string='Property', required=True)
---
+++ estate/models/estate_property_tag.py
@@ -0,0 +1,7 @@
+from odoo import fields, models
+
+class PropertyTag(models.Model):
+    _name = 'estate.property.tag'
+    _description = 'Real Estate Property Tag'
+
+    name = fields.Char('Property tag', required=True)
