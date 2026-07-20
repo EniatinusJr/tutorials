c43844b733452cd47b47332e7497a6af4daf6595
EniatinusJr <maurin@schucan.ch>
2026-07-20 13:17:48 +0200

    complete chapter 13

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Estate Account',
+    'version': '0.1',
+    'author': 'SnapErp AG',
+    'depends': [
+        'base',
+        'account'
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3'
+}
